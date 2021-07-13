2ffe6bbe684dba89aa1bf34ba2e6054befa1a67e
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-13 14:20:37 +0200

    Added Environment and Configuration services.

---
+++ DotnetToolset/Services/ConfigurationService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DotnetToolset.Services
+{
+	/// <inheritdoc />
+	public class ConfigurationService : IConfigurationService
+	{
+		private IEnvironmentService EnvironmentService { get; }
+
+		public ConfigurationService(IEnvironmentService environmentService) => EnvironmentService = environmentService;
+
+		/// <summary>
+		/// Gets the configuration
+		/// </summary>
+		/// <returns>IConfiguration compatible instance</returns>
+		public IConfiguration GetConfiguration() => GetConfiguration(Directory.GetCurrentDirectory());
+
+		/// <summary>
+		/// Gets the configuration
+		/// </summary>
+		/// <param name="basePath">Base path for binaries</param>
+		/// <returns>IConfiguration compatible instance</returns>
+		public IConfiguration GetConfiguration(string basePath) =>
+			new ConfigurationBuilder()
+				.SetBasePath(basePath)
+
+				// General settings
+				.AddJsonFile("appsettings.json", false, true)
+
+				// Environmental settings override general settings
+				.AddJsonFile($"appsettings.{EnvironmentService.EnvironmentName}.json", true, true)
+
+				// Environment variables set at machine or cloud platform override previous settings
+				.AddEnvironmentVariables()
+
+				.Build();
+	}
+}
---
+++ DotnetToolset/Services/IConfigurationService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotnetToolset.Services
+{
+	/// <summary>
+	/// Gets the app configuration based on JSON files and environment variables
+	/// </summary>
+	public interface IConfigurationService
+	{
+		/// <summary>
+		/// Gets the configuration using the current directory
+		/// </summary>
+		/// <returns></returns>
+		IConfiguration GetConfiguration();
+
+		/// <summary>
+		/// Gets the configuration using a provided base path
+		/// </summary>
+		/// <param name="basePath"></param>
+		/// <returns></returns>
+		IConfiguration GetConfiguration(string basePath);
+	}
+}

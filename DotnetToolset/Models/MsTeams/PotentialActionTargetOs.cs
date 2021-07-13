d587300872f2cb87b35e343bd7f23ce0526606bf
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-13 14:21:36 +0200

    Added the model retated to Microsoft Teams' message cards.

---
+++ DotnetToolset/Models/MsTeams/PotentialActionTargetOs.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace DotnetToolset.Models.MsTeams
+{
+	/// <summary>
+	/// Operating system targets for potential actions in message cards
+	/// </summary>
+	/// <remarks>https://docs.microsoft.com/en-us/outlook/actionable-messages/message-card-reference#actions</remarks>
+	public enum PotentialActionTargetsOs
+	{
+		/// <summary>
+		/// Any operating system
+		/// </summary>
+		[EnumMember(Value = "default")]
+		Default,
+
+		/// <summary>
+		/// Windows operating system
+		/// </summary>
+		[EnumMember(Value = "windows")]
+		Windows,
+
+		/// <summary>
+		/// iOS operating system
+		/// </summary>
+		[EnumMember(Value = "ios")]
+		Ios,
+
+		/// <summary>
+		/// Android operating system
+		/// </summary>
+		[EnumMember(Value = "android")]
+		Android
+	}
+}
---
+++ DotnetToolset/Models/MsTeams/Target.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DotnetToolset.Models.MsTeams
+{
+	public class Target
+	{
+		[JsonConverter(typeof(StringEnumConverter))]
+		public PotentialActionTargetsOs Os { get; set; }
+		public string Uri { get; set; }
+	}
+}

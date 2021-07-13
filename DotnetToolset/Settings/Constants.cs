98d8fb3c3f0544c39bf4c41ecde0b88e91fdbd60
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-13 14:19:05 +0200

    Added settings' parsers and constants.

---
+++ DotnetToolset/Settings/Constants.cs
@@ -0,0 +1,19 @@
+namespace DotnetToolset.Settings
+{
+	public static class Constants
+	{
+		public static class EnvironmentVariables
+		{
+			public const string AspnetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+			public const string DotNetRunningInContainer = "DOTNET_RUNNING_IN_CONTAINER";
+		}
+
+		public static class Environments
+		{
+			public const string Local = "local";
+			public const string Development = "development";
+			public const string Staging = "staging";
+			public const string Production = "production";
+		}
+	}
+}

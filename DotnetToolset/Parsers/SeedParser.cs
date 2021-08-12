0194b283c759e6c5f88615fb2516054271b99227
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-08-12 19:15:35 +0200

    Updated project dependencies. Added support for global data seeds.

--- DotnetToolset/Parsers/SeedParser.cs
+++ DotnetToolset/Parsers/SeedParser.cs
@@ -8,53 +8,64 @@ using System.Reflection;
 
 namespace DotnetToolset.Parsers
 {
-  public class SeedParser
-  {
-	  /// <summary>
-	  /// Gets the data to be inserted
-	  /// </summary>
-	  /// <typeparam name="T">Entity type to get data for</typeparam>
-	  /// <returns>Data enumerable</returns>
-	  public IEnumerable<T> GetSeeds<T>()
-	  {
-		  List<T> recordsList;
+	public class SeedParser
+	{
+		/// <summary>
+		/// Gets the data to be inserted
+		/// </summary>
+		/// <typeparam name="T">Entity type to get data for</typeparam>
+		/// <returns>Data enumerable</returns>
+		public IEnumerable<T> GetSeeds<T>()
+		{
+			List<T> recordsList;
 
-		  string csvPath = GetCsvPath(typeof(T).Name.Replace("Data", string.Empty));
+			string csvPath = GetCsvPath(typeof(T).Name.Replace("Data", string.Empty));
 
-		  using (StreamReader reader = new StreamReader(csvPath))
-		  {
-			  using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-			  {
-				  recordsList = csv.GetRecords<T>().ToList();
-			  }
-		  }
+			using (StreamReader reader = new StreamReader(csvPath))
+			{
+				using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+				{
+					recordsList = csv.GetRecords<T>().ToList();
+				}
+			}
 
-		  return recordsList;
-	  }
+			return recordsList;
+		}
 
-	  /// <summary>
-	  /// Gets the path to the assembly
-	  /// </summary>
-	  /// <returns></returns>
-	  public string GetAssemblyPath() => Path.GetDirectoryName(Assembly.GetAssembly(typeof(SeedParser))?.Location);
+		/// <summary>
+		/// Gets the path to the assembly
+		/// </summary>
+		/// <returns></returns>
+		private string GetAssemblyPath() => Path.GetDirectoryName(Assembly.GetAssembly(typeof(SeedParser))?.Location);
 
-	  /// <summary>
-	  /// Gets the data set name from the settings.json file
-	  /// </summary>
-	  /// <returns></returns>
-	  public string GetDataSetName()
-	  {
-		  string settingsPath = Path.Combine(GetAssemblyPath(), "settings.json");
+		/// <summary>
+		/// Gets the data set name from the settings.json file
+		/// </summary>
+		/// <returns></returns>
+		private string GetDataSetName()
+		{
+			string settingsPath = Path.Combine(GetAssemblyPath(), "settings.json");
 
-		  JObject jsonJObject = JObject.Parse(File.ReadAllText(settingsPath));
-		  return (string)jsonJObject["data-set"];
-	  }
+			JObject jsonJObject = JObject.Parse(File.ReadAllText(settingsPath));
+			return (string)jsonJObject["data-set"];
+		}
 
-	  /// <summary>
-	  /// Gets the path to the entity
-	  /// </summary>
-	  /// <param name="entityName"></param>
-	  /// <returns></returns>
-	  public string GetCsvPath(string entityName) => Path.Combine(GetAssemblyPath(), "Data", GetDataSetName(), $"{entityName}.csv");
-  }
+		/// <summary>
+		/// Gets the path to the entity
+		/// </summary>
+		/// <param name="entityName"></param>
+		/// <returns></returns>
+		private string GetCsvPath(string entityName)
+		{
+			string environmentPath = Path.Combine(GetAssemblyPath(), "Data", GetDataSetName(), $"{entityName}.csv");
+			string globalPath = Path.Combine(GetAssemblyPath(), "Data", "Global", $"{entityName}.csv");
+
+			if (File.Exists(environmentPath))
+			{
+				return environmentPath;
+			}
+
+			return globalPath;
+		}
+	}
 }

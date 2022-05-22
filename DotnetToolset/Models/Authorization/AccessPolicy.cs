1817cc263034599c719adfe325cd968070659501
Iván Sainz <ivan.sainz@aheadlabs.com>
2022-05-22 18:47:38 +0200

    Added access policy mapping class.

---
+++ DotnetToolset/Models/Authorization/AccessPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DotnetToolset.Models.Authorization
+{
+    /// <summary>
+    /// Maps an access policy from appsettings*.json
+    /// </summary>
+    public class AccessPolicy
+    {
+        /// <summary>
+        /// Access policy name
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// List of permissions for the policy
+        /// </summary>
+        public IEnumerable<string>? Permissions { get; set; }
+    }
+}
